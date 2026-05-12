f22344445e9a374d7ad8334121ca9f28d104aaa9
Βασιλης κορωναιος <vasiliskoroneos01@gmail.com>
2026-05-12 19:42:33 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.get('/api/qc-reports', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.get('/*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'login.html'));
 });
 
